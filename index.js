7ee732021cc24fd4fb97e03d748c8da0366c239c
SamBuseyne <sam.buseyne@student.ehb.be>
2021-12-15 17:00:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {
     MongoClient,
+    ObjectId
 } = require("mongodb");
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -96,7 +97,7 @@ app.get('/movie', async (req, res) => {
     console.log("API movie route called.")
 });
 
-//Save a movie to the file
+//Save a movie to the database
 app.post('/save', async (req, res) => {
     //Validation if params are missing
 
@@ -149,6 +150,80 @@ app.post('/save', async (req, res) => {
         await client.close();
     }
     console.log("API save a movie route called.")
+});
+
+//Delete a movie from the database
+app.delete('/movies/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const collection = client.db('Course_project').collection('Movies');
+
+        const query = {
+            _id: ObjectId(req.params.id)
+        };
+
+        await collection.deleteOne(query)
+        res.status(200).json({
+            succes: 'Succesfully deleted!',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        })
+    }
+})
+
+//Update a movie to the database
+app.put('/movies/:id', async (req, res) => {
+    if (!req.body._id || !req.body.movieid || !req.body.name || !req.body.director || !req.body.year || !req.body.genres || !req.body.description) {
+        res.status(400).send("Bad request, missing: id, name, points, course or session!");
+        return;
+    }
+
+    try {
+
+        await client.connect();
+
+        const collection = client.db('Course_project').collection('Movies');
+        const query = {
+            _id: ObjectId(req.params.id)
+        };
+
+        let update = {
+            $set: {
+                name: req.body.name,
+                director: req.body.director,
+                year: req.body.year,
+                genres: req.body.genres,
+                description: req.body.description,
+            }
+        };
+
+        const updateChallenge = await collection.updateOne(query, update)
+        if (updateChallenge) {
+            res.status(201).send({
+                succes: `Challenge with id "${req.body._id}" is succesfully updated!.`,
+            });
+            return;
+        } else {
+            res.status(400).send({
+                error: `Challenge with id "${req.body._id}" could not been found!.`,
+                value: error,
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
 });
 
 app.listen(port, () => {
